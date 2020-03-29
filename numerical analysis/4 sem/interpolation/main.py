0cf3ce4977472590302861b52c29bb75492ba6ab
Vladislav Saraev <saraev.v.m@gmail.com>
2020-03-29 20:40:25 +0300

    lab done

---
+++ numerical analysis/4 sem/interpolation/main.py
@@ -0,0 +1,58 @@
+import numpy as np
+from math import sin, cos, pi
+import matplotlib.pyplot as plot
+
+
+def function(t):
+    return (sin(4 * t) - t) ** 3
+
+
+def polynom(t):
+    global x, y, c
+    for i in range(len(x)):
+        if t == x[i]:
+            return y[i]
+
+    den = [c[i] / (t - x[i]) for i in range(len(c))]
+    return sum(den[i] * y[i] for i in range(len(c))) / sum(den)
+
+
+L = -2
+R = 2
+
+dots = np.linspace(L, R, 1000)
+count_of_dots = [10 * i for i in range(1, 11)]
+type_of_nodes = "equidistant"
+for l in range(2):
+    for i in range(len(count_of_dots)):
+        N = count_of_dots[i]
+        if type_of_nodes == "equidistant":
+            x = np.linspace(L, R, N)
+        else:
+            x = np.empty(N)
+            for j in range(1, N + 1):
+                x[j - 1] = (L+R)/2 + (R-L)/2*cos((2*j-1)*pi/(2*N))
+        y = np.empty(N)
+
+        for j in range(0, N):
+            y[j] = function(x[j])
+
+        c = np.ones(N)
+        for j in range(len(x)):
+            for k in range(len(x)):
+                if j != k:
+                    c[j] *= x[j] - x[k]
+            c[j] = 1 / c[j]
+        polynom_dots = [polynom(t) for t in dots]
+        function_dots = [function(t) for t in dots]
+        max_fault = max(abs(polynom_dots[i] - function_dots[i]) for i in range(len(polynom_dots)))
+        if type_of_nodes == "equidistant":
+            label_info = "Polynom in " + str(N) + " equidistant nodes (max fault =" + str(max_fault) + ")"
+        else:
+            label_info = "Polynom in " + str(N) + " chebyshev's nodes (max fault =" + str(max_fault) + ")"
+
+        plot.plot(dots, polynom_dots, label=label_info)
+        plot.plot(dots, function_dots, label="Function")
+        plot.legend()
+        plot.show()
+    type_of_nodes = "chebyshev's"
